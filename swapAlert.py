b8806b72073ca34c0c88429a013c081ea78e4253
Craig Carter <32251992+craigcarter42@users.noreply.github.com>
2018-10-29 07:32:39 -0700

    Create swapAlert.py

---
+++ swapAlert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os, sys, subprocess, re
+from subprocess import call
+
+swf = ""
+found_swf = ""
+count = 0
+
+args = str(sys.argv[1:2])
+
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+# END: notify
+for root, dirs, files in os.walk("/var/VM"):  
+    for filename in files:
+        swf = swf + filename + " "
+        if(args == "['-list']"): print("> " + filename)
+        
+found_swf = re.findall(r'swapfile.', swf)
+for f in found_swf:
+	count += 1
+	if(args == "['-swap']"): print("> " + f)
+
+if(args == "['-open']"): call(["open", "/var/VM"]); exit()
+if(args == "['-flush']"): print(" swap -- flush all swapfiles"); exit()
+if count >= 1: notify("swapAlert", str(count) + " swapfiles created")
+
+# swapfile: created
+# swapfile: changed Bigger | Smaller
+# swapfile: removed
+# swapfile: filename
+# swapfile: silent with no change
+
