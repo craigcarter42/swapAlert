07a646faa43a2101343854e449e93086f4703ce2
craigcarter42 <craigcarter42@gmail.com>
2018-12-03 18:37:34 -0800

    Compare function now compares both current swaps to plist.

--- plist.py
+++ plist.py
@@ -16,17 +16,17 @@ def error(code):
    exit()
 
 try:
-   import os, re, plistlib
+   import os, sys, re, plistlib
    from datetime import datetime
 except ImportError:
    error(1)
 
 # Path to VM folder
 # Check if folder exists
-vm_path = "/Volumes/Macintosh HD/Users/craigcarter/Projects/swapAlert/VM2/"
+vm_path = "/Volumes/Macintosh HD/Users/craigcarter/Projects/swapAlert/VM/"
 if os.path.exists(vm_path): pass
 else:
-	error(3)
+    error(3)
 
 # Path to plist file.
 # Check if file exists
@@ -34,7 +34,7 @@ else:
 plist_path = "/Volumes/Macintosh HD/Users/craigcarter/Projects/swapAlert/swaptrack.plist"
 if os.path.exists(plist_path): plist_path = os.path.expanduser('~/Projects/swapAlert/swaptrack.plist')
 else:
-	error(3)
+    error(3)
 # SETUP: END
 
 # Program starts here.
@@ -61,17 +61,17 @@ add_swaps = {}
 
 # Checks to see how many files are in VM.
 for root, dirs, files in os.walk(vm_path):
-	for f in files: file_string = file_string + f
+    for f in files: file_string = file_string + f
 
 found_swapfiles = re.findall(r'swapfile', file_string)
 len_check = len(found_swapfiles)
 if(len_check > 0):
-	for fs in found_swapfiles: final_found_swapfiles.append(fs)
+    for fs in found_swapfiles: final_found_swapfiles.append(fs)
 
-	for ffs in final_found_swapfiles[1:]:
-		final_found_swapfiles[xi] = ffs + str(i);
-		xi = xi + 1
-		i = i + 1
+    for ffs in final_found_swapfiles[1:]:
+        final_found_swapfiles[xi] = ffs + str(i);
+        xi = xi + 1
+        i = i + 1
 
 #print(final_found_swapfiles)
 
@@ -79,91 +79,129 @@ if(len_check > 0):
 # Creates dictionary with list
 for file in final_found_swapfiles:
 # Creates file path for current swapfile
-	final_vm_path = vm_path + file
+    final_vm_path = vm_path + file
 
 # Gets the last modified date/time of file
-	lastmodified = os.stat(final_vm_path).st_mtime
-	lastmod = datetime.fromtimestamp(lastmodified)
-	final_last_mod = str(lastmod)
+    lastmodified = os.stat(final_vm_path).st_mtime
+    lastmod = datetime.fromtimestamp(lastmodified)
+    final_last_mod = str(lastmod)
 
 # Gets size of current swap file
-	swapsize = os.stat(final_vm_path).st_size
-	# print(" > size: " + str(swapsize))
+    swapsize = os.stat(final_vm_path).st_size
+    # print(" > size: " + str(swapsize))
 
 # Make Swap ID for each file
-	swapid = "swf0" + str(swap_count)
+    swapid = "swf0" + str(swap_count)
 
-	add_swaps = {swapid : {"filename" : file, "lastmod" : final_last_mod, "swapsize" : swapsize}}
-	current_swaps.update(add_swaps)
+    add_swaps = {swapid : {"filename" : file, "lastmod" : final_last_mod, "swapsize" : swapsize}}
+    current_swaps.update(add_swaps)
 # Adds numbering to file names
-	swap_count += 1
+    swap_count += 1
 
 def printout(value):
-	if(value == 1):
-		for key, value in current_swaps.iteritems():
-			print(key, value)
-	if(value == 2):
-		for key, value in current_swaps.iteritems():
-			print(current_swaps.get(key))
+    print(" -- swapAlert: printout")
+    if(value == 1):
+        for key, value in current_swaps.iteritems():
+            print(key, value)
+    if(value == 2):
+        for key, value in current_swaps.iteritems():
+            print(current_swaps.get(key))
 
 def notify(title, text):
     os.system("""
               osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
 
-def compare(current_swaps, read_pl):
-	print(" -- swapAlert: Compare")
-# This method returns 0 if both dictionaries are equal, -1 if dict1 < dict2 and 1 if dict1 > dic2.
-	compared_swaps = cmp (current_swaps, read_pl)
-	if(compared_swaps == 0):
-		print(" -- swapAlert: no changes have occured")
-	elif(compared_swaps == -1):
-		print(" -- swapAlert: current_swaps is less than read_pl")
-	elif(compared_swaps == 1):
-		print(" -- swapAlert: read_pl is greater than current_swaps")
-	else:
-		print(" -- swapAlert: comparison could not be completed")
+def compare(dict10,dict11):
+    print(" -- swapAlert: Compare")
+    key_count = 0
+    dict10_len = len(dict10)
+    dict11_len = len(dict11)
+    while True:
+        if(dict10_len > 0):
+            dict10_len = dict10_len - 1
+            dict11_len = dict11_len - 1
 
-	# compared_swaps = {}
-	# for key in current_swaps.keys():
-	# 	if key in read_pl.keys():
-	# 		if current_swaps[key] == read_pl[key]:
-	# 			compared_swaps[key] = current_swaps[key]
+            key_name = "swf0" + str(key_count)
 
-	# for key, value in compared_swaps.items():
-	#     print(key)
-	#     print(value)
+            dict10_filename = dict10.get(key_name)["filename"]
+            dict11_filename = dict11.get(key_name)["filename"]
+
+            # LASTMOD
+            print(" -- lastmod:")
+            print(str(key_name) + " : " + dict10_filename + ": " + str(dict10.get(key_name)["lastmod"]))
+            print(str(key_name) + " : " + dict10_filename + ": " + str(dict11.get(key_name)["lastmod"]))
+
+            dict10_lastmod = dict10.get(key_name)["lastmod"]
+            dict11_lastmod = dict11.get(key_name)["lastmod"]
+            if(dict10_lastmod == dict11_lastmod):
+                print(str(key_name) + " -- lastmod: No Changes")
+            elif(dict10_lastmod > dict11_lastmod):
+                print(str(key_name) + " : " + str(dict10_filename) + " was modified")
+            else:
+                print(str(key_name) + " : " + str(dict11_filename) + " was modified")
+
+            # SWAPSIZE
+            print(" -- swapsize:")
+            print(str(key_name) + " : " + dict10_filename + ": " + str(dict10.get(key_name)["swapsize"]))
+            print(str(key_name) + " : " + dict11_filename + ": " + str(dict11.get(key_name)["swapsize"]))
+
+            dict10_swapsize = dict10.get(key_name)["swapsize"]
+            dict11_swapsize = dict11.get(key_name)["swapsize"]
+            if(dict10_swapsize == dict11_swapsize):
+                print(str(key_name) + " -- swapsize: No Changes")
+            elif(dict10_swapsize > dict11_swapsize):
+                print(str(key_name) + " : " + str(dict10_filename) + " swapfile size has increased")
+            else:
+                print(str(key_name) + " : " + str(dict11_filename) + " swapfile size has decreased")
+
+            print("")
+            key_count = key_count + 1
+        else:
+            print("END:")
+            break
 
 # Write to plist file
 def write_plist():
-	global current_swaps
-	print(" -- swapAlert: write_plist")
-	# for key, value in current_swaps.iteritems():
-		# pl = key, value
-		# print(pl)
-	pl = current_swaps
-	plistlib.writePlist(pl, plist_path)
+    global current_swaps
+    print(" -- swapAlert: write_plist")
+    # for key, value in current_swaps.iteritems():
+        # pl = key, value
+        # print(pl)
+    #pl = current_swaps
+    plistlib.writePlist(current_swaps, plist_path)
 
 def read_plist():
-	global read_pl
-	print(" -- swapAlert: read_plist")
-	read_pl = plistlib.readPlist(plist_path)
-	for key, value in read_pl.iteritems():
-		print(key, value)
+    global read_pl
+    print(" -- swapAlert: read_plist")
+    read_pl = plistlib.readPlist(plist_path)
+    # for key, value in read_pl.iteritems():
+    #     print(key, value)
+    print(read_pl)
 
 def main():
-	# write_plist()
-	print(" > main")
-	read_plist()
-	compare(current_swaps, read_pl)
-	#read_plist()
-	# printout(1)
+    print(" -- swapAlert: main")
+    args = sys.argv[1:]
+    if(len(args) > 0):
+        for a in args:
+            final_args = a
+
+        if(final_args == "w"): write_plist()
+        elif(final_args == "r"): read_plist()
+        elif(final_args == "p"): printout(1)
+
+    read_plist()
+    compare(current_swaps, read_pl)
+    #compare(current_swaps, read_pl)
+    # write_plist()
+    
+    # printout(1)
 
 
 # NEXTUP:
 # Consolidate all swapfile/s information into single dictionary.
-# Read plist file.
 # Compare current swapfile/s to plist.
+# Read plist file.
 # Notify user if needed.
 # Update plist file.
 
