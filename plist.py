11078930a5127da8bba2d020bfb04cf38eb386cc
craigcarter42 <craigcarter42@gmail.com>
2018-12-04 07:09:02 -0800

    Cleaned up code, removed old vars and comments. Added a mode feature for when printing to the screen.
    New Feature:
     - Added mode
     - Added args
     - plist check: If no plist is found program generates a new file.
     - read_plist now uses mode when called to determine print status.
    
    Updates Needed:
     - Write plist_check method to handle:
     -- Loading current plist
     -- Creating template plist
     -- Deleting damage plist and creating new plist
    
    Issue Found:
     0001  - compare: Unable to handle empty plist files.

--- plist.py
+++ plist.py
@@ -16,7 +16,7 @@ def error(code):
    exit()
 
 try:
-   import os, sys, re, plistlib
+   import os, sys, re, time, plistlib
    from datetime import datetime
 except ImportError:
    error(1)
@@ -31,29 +31,24 @@ else:
 # Path to plist file.
 # Check if file exists
 # Attempt to generate new plist if file not found.
+template_plist = "String Holder"
 plist_path = "/Volumes/Macintosh HD/Users/craigcarter/Projects/swapAlert/swaptrack.plist"
 if os.path.exists(plist_path): plist_path = os.path.expanduser('~/Projects/swapAlert/swaptrack.plist')
 else:
+    plistlib.writePlist(template_plist, plist_path)
     error(3)
 # SETUP: END
 
-# Program starts here.
-print(" -- swapAlert: Started")
-
 # Declare variables:
-swapfiles = []
-file_list = []
-past_dict = {}
-read_pl = {}
-
-final_found_swapfiles = []
-
 i = 0
 xi = 1
-
 swap_count = 0
+read_pl = {}
+final_found_swapfiles = []
 file_string = ""
 swapid = ""
+print_mode = "v"
+logging_mode = "v"
 
 # Used for current swapfiles in VM folder.
 current_swaps = {}
@@ -66,14 +61,13 @@ for root, dirs, files in os.walk(vm_path):
 found_swapfiles = re.findall(r'swapfile', file_string)
 len_check = len(found_swapfiles)
 if(len_check > 0):
-    for fs in found_swapfiles: final_found_swapfiles.append(fs)
+    for fs in found_swapfiles:
+        final_found_swapfiles.append(fs)
 
     for ffs in final_found_swapfiles[1:]:
         final_found_swapfiles[xi] = ffs + str(i);
         xi = xi + 1
         i = i + 1
-
-#print(final_found_swapfiles)
 
 # Gets file name, last modified, and size.
 # Creates dictionary with list
@@ -108,12 +102,14 @@ def printout(value):
             print(current_swaps.get(key))
 
 def notify(title, text):
+    print(" -- swapAlert: notify")
     os.system("""
               osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
+    time.sleep(2)
 
 def compare(dict10,dict11):
-    print(" -- swapAlert: Compare")
+    print(" -- swapAlert: compare")
     key_count = 0
     dict10_len = len(dict10)
     dict11_len = len(dict11)
@@ -137,9 +133,12 @@ def compare(dict10,dict11):
             if(dict10_lastmod == dict11_lastmod):
                 print(str(key_name) + " -- lastmod: No Changes")
             elif(dict10_lastmod > dict11_lastmod):
+                notify("swapAlert2", str(key_name) + " : " + str(dict10_filename) + " was modified")
                 print(str(key_name) + " : " + str(dict10_filename) + " was modified")
             else:
+                notify("swapAlert2", str(key_name) + " : " + str(dict11_filename) + " was modified")
                 print(str(key_name) + " : " + str(dict11_filename) + " was modified")
+
 
             # SWAPSIZE
             print(" -- swapsize:")
@@ -151,8 +150,10 @@ def compare(dict10,dict11):
             if(dict10_swapsize == dict11_swapsize):
                 print(str(key_name) + " -- swapsize: No Changes")
             elif(dict10_swapsize > dict11_swapsize):
+                notify("swapAlert2", str(key_name) + " : " + str(dict10_filename) + " swapfile size has increased")
                 print(str(key_name) + " : " + str(dict10_filename) + " swapfile size has increased")
             else:
+                notify("swapAlert2", str(key_name) + " : " + str(dict11_filename) + " swapfile size has decreased")
                 print(str(key_name) + " : " + str(dict11_filename) + " swapfile size has decreased")
 
             print("")
@@ -161,26 +162,36 @@ def compare(dict10,dict11):
             print("END:")
             break
 
+# Write to plist file at end to save results for next cycle
+
 # Write to plist file
 def write_plist():
     global current_swaps
     print(" -- swapAlert: write_plist")
-    # for key, value in current_swaps.iteritems():
-        # pl = key, value
-        # print(pl)
-    #pl = current_swaps
     plistlib.writePlist(current_swaps, plist_path)
 
-def read_plist():
+def read_plist(mode):
+# Silent Mode: reads plist but give no output
+# Verbose Mode: reads plist and gives output
+# Default Mode: Unknown args provide, defaults to verbose
     global read_pl
-    print(" -- swapAlert: read_plist")
     read_pl = plistlib.readPlist(plist_path)
-    # for key, value in read_pl.iteritems():
-    # 	print(key, value)
-    print(read_pl)
+    if(mode == "v"): print(" -- swapAlert: read_plist / verbose\n"); print(read_pl)
+    elif(mode == "s"): print(" -- swapAlert: read_plist / silent")
+    else: print(" -- swapAlert: read_plist / default")
+
+def args():
+    global print_mode
+    global logging_mode
+# Print Mode: (s) Silent (v) Verbose (*) Default
+# Logging Mode: (s) Silent (v) Verbose (c) Critical (*) Default
+    print(" -- swapAlert: args")
+    
 
 def main():
     print(" -- swapAlert: main")
+    print(logging_mode)
+    print(print_mode)
     args = sys.argv[1:]
     if(len(args) > 0):
         for a in args:
@@ -190,11 +201,9 @@ def main():
         elif(final_args == "r"): read_plist()
         elif(final_args == "p"): printout(1)
 
-    read_plist()
+    read_plist("s")
     compare(current_swaps, read_pl)
-    #compare(current_swaps, read_pl)
     # write_plist()
-
     # printout(1)
 
 
@@ -211,7 +220,8 @@ def main():
 # Add CLI.
 
 if __name__ == '__main__':
-   main()
+    print(" -- swapAlert: Started")
+    main()
 
 
 
